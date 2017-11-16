d9efa9f66a9963957679170eec1112f3e8576991
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-11-16 10:29:57 +0100

    create reducers students

---
+++ src/reducers/students.js
@@ -0,0 +1,12 @@
+
+
+const students = [
+  {
+    name: "Wayne Louwrens",
+    photo: "https://image.flaticon.com/icons/svg/145/145859.svg",
+  },
+  {
+    name: "Marco Woud",
+    photo: "https://image.flaticon.com/icons/svg/145/145867.svg",
+  },
+]
